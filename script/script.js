57e86c7915752933abc5b29544acb93867561788
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-10-17 23:20:57 +0400

    Залил игру

--- script/script.js
+++ script/script.js
@@ -1,29 +1,63 @@
 'use strict';
-
-let mathRandom = Math.floor(Math.random() * 100 + 1);
+let random = Math.floor(Math.random() * 100 + 1);
+console.log(random);
+let count = 10;
 
 function quessNumber() {
-    let namber = mathRandom;
+    
+    let namber = random; 
+    console.log(namber);       
     let question = prompt('Введите число'); 
     
-    if (isNaN(question) || question === '') {         
+    
+    if (question === null){
+        alert('Игра окончена!!!');
+        return
+
+    } else if (isNaN(question) || question.trim() === '') {         
         alert('Введите число');
         return quessNumber();
-
-    } else if (question === null){
-        return alert('Игра окончена!!!');
        
     } else if (question > namber){
-        alert('Загаданное число меньше');
-        return quessNumber();
+        
+        alert('Загаданное число меньше ' + 'осталось попыток ' + --count);
+        
+        if (count === 0) {
+            let ansver = confirm('Попытки закончились, хотите сыграть еще?');
+            if (ansver === true){                
+                return newGame(), count = 10, quessNumber();
+            } else {
+                return
+            }
+        } else {
+            
+            return quessNumber();
+        }        
 
-    } else if (question < namber){
-        alert('Загаданное число больше');
-        return quessNumber();
+    } else if (question < namber){        
+        alert('Загаданное число больше ' + 'осталось попыток ' + --count);
+        if (count === 0) {
+            let ansver = confirm('Попытки закончились, хотите сыграть еще?');
+            if (ansver === true){                
+                return newGame(), count = 10, quessNumber();
+
+            } else {
+                return
+            }
+        } else {
+            
+            return quessNumber();
+        }
+        
 
     } else {        
-        alert('Поздравляю, Вы угадали!!!');
-        
+            let ansver = confirm('Поздравляю Вы угадали, хотите сыграть еще?');
+            if (ansver === true){                
+                return newGame(), count = 10, quessNumber();
+
+            } else {
+                return
+            }
         
     } 
     
@@ -31,10 +65,18 @@ function quessNumber() {
      
 
 }
+function newGame() {
+    random = Math.floor(Math.random() * 100 + 1);
+        
+       
+};
+
+
 
 quessNumber();
+newGame();
 
-    
+ 
 
 
 
