c990ea9a3a9e934fdce3fd09b623e13707011649
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-10-18 00:44:47 +0400

    Добавил 7 усложненное

--- script/script.js
+++ script/script.js
@@ -1,81 +1,27 @@
 'use strict';
-let random = Math.floor(Math.random() * 100 + 1);
-console.log(random);
-let count = 10;
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const day = document.getElementById('fordays');
+const todayDay = new Date();
 
-function quessNumber() {
-    
-    let namber = random; 
-    console.log(namber);       
-    let question = prompt('Введите число'); 
-    
-    
-    if (question === null){
-        alert('Игра окончена!!!');
-        return
-
-    } else if (isNaN(question) || question.trim() === '') {         
-        alert('Введите число');
-        return quessNumber();
-       
-    } else if (question > namber){
-        
-        alert('Загаданное число меньше ' + 'осталось попыток ' + --count);
-        
-        if (count === 0) {
-            let ansver = confirm('Попытки закончились, хотите сыграть еще?');
-            if (ansver === true){                
-                return newGame(), count = 10, quessNumber();
-            } else {
-                return
-            }
-        } else {
-            
-            return quessNumber();
-        }        
-
-    } else if (question < namber){        
-        alert('Загаданное число больше ' + 'осталось попыток ' + --count);
-        if (count === 0) {
-            let ansver = confirm('Попытки закончились, хотите сыграть еще?');
-            if (ansver === true){                
-                return newGame(), count = 10, quessNumber();
-
-            } else {
-                return
-            }
-        } else {
-            
-            return quessNumber();
+function days() {
+    week.forEach((item, i) => {
+        let newdiv = document.createElement('div'); 
+        if (i === +todayDay.getDay()-1) { 
+            console.log(todayDay.getDay());
+            newdiv.classList.add('today');
+            newdiv.textContent = week[i];
         }
-        
+        if (item == 'Суббота' || item == 'Воскресенье') {
+            newdiv.classList.add('italic');
+            newdiv.textContent = week[i];
+        } else {
+            newdiv.textContent = week[i];
+        }
+        day.appendChild(newdiv);
 
-    } else {        
-            let ansver = confirm('Поздравляю Вы угадали, хотите сыграть еще?');
-            if (ansver === true){                
-                return newGame(), count = 10, quessNumber();
-
-            } else {
-                return
-            }
-        
-    } 
-    
-
-     
-
-}
-function newGame() {
-    random = Math.floor(Math.random() * 100 + 1);
-        
-       
+    });
 };
-
-
-
-quessNumber();
-newGame();
-
+days();
  
 
 
